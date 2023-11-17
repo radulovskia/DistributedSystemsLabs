95a931ff84b728a51f2161b7508a2c242f5b15f8
radulovskia <85944330+somefinkistudent@users.noreply.github.com>
2023-11-17 22:08:00 +0100

    Mini fix in lab 1

--- lab/lab1/UDP_Client.java
+++ lab/lab1/UDP_Client.java
@@ -22,7 +22,7 @@ class SenderWorker extends Thread{
     public void run(){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try{
-            System.out.println(get_timestamp() + "\nlab1.Sender worker started.");
+            System.out.println(get_timestamp() + "\nSender worker started.");
             String line;
             while((line = br.readLine()) != null){
                 System.out.println(get_timestamp() + "\nSending: " + line);
@@ -56,7 +56,7 @@ class ReceiverWorker extends Thread{
     @Override
     public void run(){
         try{
-            System.out.println(get_timestamp() + "\nlab1.Receiver worker started.");
+            System.out.println(get_timestamp() + "\nReceiver worker started.");
             while(true){
                 p = new DatagramPacket(buffer, buffer.length);
                 socket.receive(p);
